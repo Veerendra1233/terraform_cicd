0d740dd04d543392b7c1f5a13edac141faea0b75
Veerendra1233 <veerendra2koncha@gmail.com>
2025-04-29 09:58:52 +0530

    Update-add s3 for statefile main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-bucket"  # Replace with your S3 bucket name
+    key    = "path/to/terraform.tfstate"  # Define path where state will be stored
+    region = "us-east-2"                  # Replace with your AWS region
+    encrypt = true                        # Enable encryption for security
+    acl     = "private"                   # Keep the state file private
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
