8bc71a71a39703eb8d0b65c4f949a6e88e93828c
Veerendra1233 <veerendra2koncha@gmail.com>
2025-04-29 10:33:33 +0530

    Update chnged s3 west-2 main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "my-terraform-state-bucket"  # Replace with your S3 bucket name
     key    = "path/to/terraform.tfstate"  # Define path where state will be stored
-    region = "us-east-2"                  # Replace with your AWS region
+    region = "us-west-2"                  # Replace with your AWS region
     encrypt = true                        # Enable encryption for security
     acl     = "private"                   # Keep the state file private
   }
