fde5c6c222a862ec80f142d1ac33caa2cf1515c1
Veerendra1233 <veerendra2koncha@gmail.com>
2025-04-29 09:34:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ resource "aws_instance" "example" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
-  vpc_security_group_ids = [aws_security_group.example_sg.id]
 
   tags = {
     Name = var.instance_name
